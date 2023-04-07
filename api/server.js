39a2e7357a7f17dfc7184cad38d1a577a9736031
Eshgin Farzaliyev <eshqinferzeliyev@gmail.com>
2023-04-08 00:40:00 +0400

    add/api

--- api/server.js
+++ api/server.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const mongoose = require("mongoose");
 const path = require("path");
+const BookStore =require("./models/BookModel");
 
 
 
@@ -12,16 +13,38 @@ const app =express();
 app.use(bodyParser.json())
 app.use(cors());
 
-// app.get('/', (req,res)=>{
-//     res.send("WELCOME TO FIRST PAGE EXPRESS")
-// })
-// app.get('/news', (req,res)=>{
-//     res.send(`<h1>news</h1>`)
-// })
+//mongoose connection
+mongoose.connect("mongodb+srv://eshgnfrz:p6AamnHHGdAPXqpq@cluster0.ooaouuh.mongodb.net/?retryWrites=true&w=majority", {
+   useNewUrlParser: true,
+   useUnifiedTopology:true,
+   useCreateIndex:true})
+   .then(console.log("Connected to Database"))
+   .catch((err)=> console.log(err))
+
+
+app.post('./newbook', async (req, res)=>{
+    try{
+const newBook = new BookStore({
+    bookName:req.body.bookName,
+    author:req.body.author,
+    quantity:req.body.quantity,
+    deparment:req.body.deparment,
+    comments:req.body.comments
+})
+
+const book = await newBook.save();
+res.status(200).json
+
+
+
+    } catch(err){
+        console.log(err)
+    }
+})
 
 
 app.listen(5000, ()=>{
-    console.log("SERVER WORK")
+    console.log("SERVER is WORK")
 })
 
 
